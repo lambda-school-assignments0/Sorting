eecfd51bf75968ebac1c0b70df9cdf9b52b4a7eb
schang1146 <sammychang2185@gmail.com>
2020-06-11 13:29:02 -0700

    finished merge_sort_in_place and merge_in_place

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -34,11 +34,45 @@ def merge_sort( arr ):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
+    # initialize pointer to start at "2nd" array
+    start2 = mid + 1
+
+    # the two arrays should both be sorted, check if end value
+    # of "first" array is less than "second" array
+    # if true, then overall array should also be sorted
+    if arr[mid] <= arr[start2]:
+        return arr
+
+    # traverse through arrays and compare values to see where
+    # to switch values
+    while start <= mid and start2 <= end:
+        # if out of order
+        if arr[start2] < arr[start]:
+            # swap and shift everything to the right so we keep it
+            # sorted
+            arr[:] = arr[0:start] + [arr[start2]] + arr[start:start2] + arr[start2+1:]
+            # increment pointers
+            start += 1
+            start2 += 1
+            # (mid needs to incrememnt because we shifted to the right)
+            mid += 1
+        # start element is in the right place
+        else:
+            start += 1
 
     return arr
 
+
 def merge_sort_in_place(arr, l, r): 
     # TO-DO
+    # get middle
+    if l < r:
+        m = (l + r) // 2
+
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+
+        merge_in_place(arr, l, m, r)
 
     return arr
 
